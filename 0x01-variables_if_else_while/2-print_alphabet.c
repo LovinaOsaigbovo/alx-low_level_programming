97fbc5710a38feb4e1b5e0d3e5dc23880ae357e5
LovinaOsaigbovo <mails4vina@gmail.com>
2022-09-09 16:34:47 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,29 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-/**
-  * main - Entry point
-  *
-  * Description: 'the program's description'
-  *
-  * Return: Always 0 (Success)
-  */
-int main(void)
 {
-	int n;
-	srand(time(0));
-	if (n > 0)
+	char alphabet;
+
+	for(alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
-		printf("%d is positive\n", n);
+		putchar(alphabet);
 	}
-	if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
-}
