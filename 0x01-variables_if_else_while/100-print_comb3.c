e6136d0d7601fbb5bc765efa4c3a99ebcefb44ac
LovinaOsaigbovo <mails4vina@gmail.com>
2022-09-11 11:59:00 -0700

    Hmmmn

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 48; a <=57; a++)
+	for (a = 48; a <= 57; a++)
 	{
 		 for (b = 49; b <= 57; b++)
 		 {
-			  if ( a != b && b > a)
+			  if (a != b && b > a)
 			  {
 				  putchar(a);
 				  putchar(b);
