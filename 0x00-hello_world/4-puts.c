6b76f1acea445970d65ca0b6ede2130ea324f798
LovinaOsaigbovo <mails4vina@gmail.com>
2022-09-11 14:58:53 -0700

    Debugging 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,11 @@
  *Return: By default returns zero if no erros otherwise returns non-zero value.
  */
 int main(void)
+
 {
+	
 		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+
+				return (0);
+
 }
