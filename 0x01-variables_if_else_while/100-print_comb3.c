6cf891393db181e5cc13833f0eae9014cb76b393
LovinaOsaigbovo <mails4vina@gmail.com>
2022-09-11 11:13:59 -0700

    Debugging 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Description: 'to print all possible different combinations of two digits'
- *
- * Return: Always 0 (Success)
+ * main - program compilation begins from main
+ * Description: Program to print all possible combinations of numbers with no two numbers same in pairin
+ *  * Return: 0 means success
  */
 int main (void)
 {
-	int a;
-	int b;
+	int a, b;
 
 	(for a = 48; a <= 56; a++)
 	{
 		(for b = 49; b <= 57; b++)
 		{
-			if (b > a)
+			if ( a != b && b > a)
 			{
 				putchar (a);
 				putchar (b);
-				if (a != 56 || b != 57)
-				{
-					putchar(',');
-						putchar(' ');
+				putchar(',');
 				}
 			}
 		}
 	}
-	putchar('\n');
+	putchar('\n')
 	return (0);
+	
 }
