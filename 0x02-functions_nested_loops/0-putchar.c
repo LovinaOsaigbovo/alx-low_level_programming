d5dad3b845c2fdd4c4787a60576cd24aafcb4dd7
LovinaOsaigbovo <mails4vina@gmail.com>
2022-09-13 15:19:56 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * comments really don't matter
+ *
+ * anyway this c-file is to print characters
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
