9f7dff290547507c2272a0f0f4baf7b989df7522
Lovina Osaigbovo <111295018+LovinaOsaigbovo@users.noreply.github.com>
2022-09-26 19:30:31 -0700

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,45 +1,20 @@
 #include "main.h"
-#define NULL 0
 
 /**
-* _strstr - locate and return pointer to first occurence of substring
-* @haystack: string to search
-* @needle: target substring to search for
-* Return: pointer to index of string at first occurence of whole substring
-*/
-char *_strstr(char *haystack, char *needle)
+ * print_chessboard - function that prints a chessboard
+ * @a:  the 2D array to print
+ */
+
+void print_chessboard(char (*a)[8])
 {
-	int i = 0, j, x;
+	int i, j;
 
-	if (needle[0] == '\0')
-		return (haystack);
-
-	while (haystack[i] != '\0') /* iterate through haystack */
+	for (i = 0; i < 8; i++)
 	{
-		/* if a byte matches first char of needle */
-		/* interate through needle until match ends */
-
-		if (haystack[i] == needle[0])
+		for (j = 0; j < 8; j++)
 		{
-			x = i, j = 0;
-
-			while (needle[j] != '\0')
-			{
-				if (haystack[x] == needle[j])
-				x++, j++;
-
-				else
-				break;
-			} /* if matched throughout, return haystack */
-
-			if (needle[j] == '\0')
-			{
-				return (haystack + i);
-			}
+			_putchar(a[i][j]);
 		}
-
-		i++;
+		_putchar('\n');
 	}
-
-	return (NULL); /* No match */
 }
