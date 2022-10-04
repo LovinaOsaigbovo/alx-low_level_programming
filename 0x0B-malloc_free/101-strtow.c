7d5a25fd5ce1c13ad12d53062b0b796a63345e0d
LovinaOsaigbovo <mails4vina@gmail.com>
2022-10-04 09:33:53 -0700

    101-strtow.c re-written

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * ch_free_grid - frees a 2 dimensional array.
+ * ch_free_grid - frees a 2 dimensional array
  * @grid: multidimensional array of char.
  * @height: height of the array.
  *
@@ -18,6 +18,7 @@ void ch_free_grid(char **grid, unsigned int height)
 		free(grid);
 	}
 }
+
 
 /**
  * strtow - splits a string into words.
@@ -44,25 +45,24 @@ char **strtow(char *str)
 	for (i = a1 = 0; i < height; i++)
 	{
 		for (c = a1; str[c] != '\0'; c++)
-				{
-				if (str[c] == ' ')
+		{
+			if (str[c] == ' ')
 				a1++;
-				if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-				{
+			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			{
 				aout[i] = malloc((c - a1 + 2) * sizeof(char));
 				if (aout[i] == NULL)
 				{
-				ch_free_grid(aout, i);
-				return (NULL);
+					ch_free_grid(aout, i);
+					return (NULL);
 				}
 				break;
-				}
-				
-				for (j = 0; a1 <= c; a1++, j++)
-				aout[i][j] = str[a1];
-				aout[i][j] = '\0';
-				}
-				aout[i] = NULL;
-				return (aout);
+			}
+		}
+		for (j = 0; a1 <= c; a1++, j++)
+			aout[i][j] = str[a1];
+		aout[i][j] = '\0';
 	}
+	aout[i] = NULL;
+	return (aout);
 }
